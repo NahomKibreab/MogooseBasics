4cbc7189ce0892166dbe1b72fcc0348998ae1059
NahomKibreab <nknahom@gmail.com>
2021-02-14 23:54:56 -0700

    Adding Model Static Methods

--- product.js
+++ product.js
@@ -50,6 +50,10 @@ productSchema.methods.addCategory = function (newCat) {
   return this.save();
 };
 
+productSchema.statics.fireSale = function () {
+  return this.updateMany({}, { onSale: true, price: 0 });
+};
+
 const Product = mongoose.model('Product', productSchema);
 
 const findProduct = async () => {
@@ -62,6 +66,7 @@ const findProduct = async () => {
 };
 
 findProduct();
+Product.fireSale().then((res) => console.log(res));
 
 // const bike = new Product({
 //   name: 'Road Bike',
